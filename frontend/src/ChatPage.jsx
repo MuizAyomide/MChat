852c0ee158baea8c4de2317145e378bfc5d61cbc
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-06-24 01:46:55 +0100

    commit

--- frontend/src/ChatPage.jsx
+++ frontend/src/ChatPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
-const ChatPage = () => {
-  return <div>Chats....</div>;
+import {PrettyChatWindow} from 'react-chat-engine-pretty'
+const ChatPage = (props) => {
+  return <div style={{height: '100vh'}}>
+    <PrettyChatWindow
+    projectId="6fc90138-9829-4f0c-8a14-c2f1d7a6b4bd"
+    username={props.user.username}
+    secret={props.user.secret}
+    style={{height: '100%'}}
+    />
+  </div>;
 };
 
 export default ChatPage;
